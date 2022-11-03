dc612a0306b95785970fdb43314b59cd7319e14e
Lê Minh Vinh <mvinhle22@gmail.com>
2022-11-03 17:53:41 +0700

    build remote

--- ft_build_web.sh
+++
@@ -1,7 +0,0 @@
-cd vinhmdev
-flutter clean
-flutter pub get
-flutter build web
-rm -rfv ../web/
-cp -rfv ./build/web ../
-cd ..
--- vinhmdev/android/app/src/main/kotlin/com/example/vinhmdev/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.vinhmdev
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
