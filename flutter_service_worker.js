98d006c287701de12910457eb9dbf5f80de5e412
Lê Minh Vinh <mvinhle22@gmail.com>
2022-12-10 19:46:40 +0700

    build vinhmdev

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -15,7 +15,7 @@ const RESOURCES = {
 "addon/html-coverimage/images/megumin.jpg": "f525ef62e87bf9f9396e3abaf125955d",
 "addon/html-coverimage/images/megumin2.png": "d7d08106970dc08772884f848244d118",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"index.html": "919eb3e0350129eca075b8affc7a0ff7",
+"index.html": "33e3e1859c0441bfb09aa8be82e82205",
 "work_tp/toplinksale/www/icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "work_tp/toplinksale/www/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "work_tp/toplinksale/www/icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -49,13 +49,13 @@ const RESOURCES = {
 "canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
 "canvaskit/canvaskit.wasm": "bf50631470eb967688cca13ee181af62",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
-"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"assets/AssetManifest.json": "00c1db317c327e87e716631e517fecbf",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/shaders/ink_sparkle.frag": "4bf9c21bdd54a21ca371a65664b0e2c7",
+"assets/assets/images/error.jpeg": "d73c497c2a75b90278013b2a9001f3a0",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/NOTICES": "fb9e9695eff13a72ecc9316559bef51f",
-"assets/images/error.jpeg": "d73c497c2a75b90278013b2a9001f3a0",
-"main.dart.js": "6154ca0b020649c29f8bd6d6f3f5e75a",
+"assets/NOTICES": "72fa396b5dc31ff82a50b321782e2cfa",
+"main.dart.js": "34e806148e66b8b028a726fefc2c5552",
 "manifest.json": "e5f198bab29058ab3ae3dee6f2162e62"
 };
 
