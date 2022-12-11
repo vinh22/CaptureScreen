1a9fba30ca3fb84fae7a2ec3e579e0276b7e25fc
Lê Minh Vinh <mvinhle22@gmail.com>
2022-12-12 01:42:13 +0700

    build vinhmdev

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -42,6 +42,7 @@ const RESOURCES = {
 "work_tp/mts/captchav3.html": "32f789047e53f9437a3b35cd13c372ac",
 "work_tp/mts/chat_bot.html": "ab1e4d8332fb06b3910957f92c9dc209",
 "work_tp/mts/mastercard_payment.html": "2ba7107bf2e573dacbe07f3b9361c50e",
+"firebase-messaging-sw.js": "cc57d3514ce89540950b08a16df3c0a6",
 "version.json": "584e0b2fb16ae04941658488cfdc8dab",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "canvaskit/profiling/canvaskit.js": "38164e5a72bdad0faa4ce740c9b8e564",
@@ -57,7 +58,7 @@ const RESOURCES = {
 "assets/assets/images/error.jpeg": "d73c497c2a75b90278013b2a9001f3a0",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/NOTICES": "c11b3d0753dce536b2e1b121471c1454",
-"main.dart.js": "87e11fd6979602ae524075dea22dd796",
+"main.dart.js": "fcd091750f354e11776c1c9208346879",
 "manifest.json": "4c233020f0e17529a41d9434233a8d93"
 };
 
