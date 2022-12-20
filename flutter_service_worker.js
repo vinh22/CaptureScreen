6a93ccbae5108d95f93a3001ced3606ed73948ec
Lê Minh Vinh <mvinhle22@gmail.com>
2022-12-20 23:45:30 +0700

    build vinhmdev

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -75,7 +75,7 @@ const RESOURCES = {
 "work_tp/mts/captchav3.html": "32f789047e53f9437a3b35cd13c372ac",
 "work_tp/mts/chat_bot.html": "ab1e4d8332fb06b3910957f92c9dc209",
 "work_tp/mts/mastercard_payment.html": "2ba7107bf2e573dacbe07f3b9361c50e",
-"firebase-messaging-sw.js": "aa35aecc259ff8b152fb9ec5a903e434",
+"firebase-messaging-sw.js": "3b79ca9aacffe5575cb4d9aaabf89513",
 "version.json": "584e0b2fb16ae04941658488cfdc8dab",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "canvaskit/profiling/canvaskit.js": "38164e5a72bdad0faa4ce740c9b8e564",
