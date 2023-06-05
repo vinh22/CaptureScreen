c4e300fbca4de49f6ab6f23bbb19d962e5845679
Lê Minh Vinh <mvinhle22@gmail.com>
2023-06-05 12:12:56 +0700

    ---

--- addon/pdf-scanner/flutter_service_worker.js
+++ addon/pdf-scanner/flutter_service_worker.js
@@ -3,20 +3,25 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "version.json": "6899d14c6ce835b73f7890c02a1b0f7c",
-"main.dart.js": "9b322a484d2f47a72b2dccda06cc23bf",
+  "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
+"version.json": "6899d14c6ce835b73f7890c02a1b0f7c",
+"main.dart.js": "2bd7f1b061b82f45105937e865aa3b36",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/NOTICES": "14455b56385aae922372c6026fabca05",
 "assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "manifest.json": "d5f6d55972e4ff1b2cbf846942489dd3",
-"index.html": "44e8477e8b0155915346c7c2b6b8cbb0",
-"/": "44e8477e8b0155915346c7c2b6b8cbb0",
+"index.html": "a8a6b1a0bccaffea0aacc35f65e1e283",
+"/": "a8a6b1a0bccaffea0aacc35f65e1e283",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
+"canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
+"canvaskit/canvaskit.wasm": "3de12d898ec208a5f31362cc00f09b9e",
+"canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796"
 };
 
